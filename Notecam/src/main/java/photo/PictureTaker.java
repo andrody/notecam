c6ad336e13c3e3da9cc5e5d942ac49c7143ad713
Bruno Martins <bruno963852@gmail.com>
2014-05-03 18:53:43 -0300

    Adicionado if (result_ok) no onActivityResult do picture taker

--- Notecam/src/main/java/photo/PictureTaker.java
+++ Notecam/src/main/java/photo/PictureTaker.java
@@ -51,59 +51,55 @@ public class PictureTaker {
 
     //Chame obrigatoriamente esse método dentro do OnActivityResult da mesma activity
     //na qual vc chamou o takePicture
-    public void OnActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        // Determina o caminho e os metadados da foto
-        final long currentTimeMillis = System.currentTimeMillis();
-        final String appName = activity.getString(R.string.app_name);
-        final String galleryPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
-        final String albumPath = galleryPath + "/" + appName + "/" + folderName;
-        final String photoPath = albumPath + "/" + currentTimeMillis + ".png";
-        final ContentValues values = new ContentValues();
-        values.put(MediaStore.MediaColumns.DATA, photoPath);
-        values.put(MediaStore.Images.Media.MIME_TYPE, PHOTO_MIME_TYPE);
-        values.put(Images.Media.TITLE, appName);
-        values.put(Images.Media.DESCRIPTION, appName);
-        values.put(Images.Media.DATE_TAKEN, currentTimeMillis);
-        // Cria o diretório
-        File album = new File(albumPath);
-        if (!album.isDirectory() && !album.mkdirs())
-        {
-            Log.e(TAG, "Failed to create album directory at " + albumPath);
-            return;
-        }
-        // Pega a foto
-        Bundle extras = data.getExtras();
-        Bitmap imageBitmap = (Bitmap) extras.get("data");
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, bytes);
-        //Salva a foto no arquivo
-        try {
-            album.createNewFile();
-            FileOutputStream fo = new FileOutputStream(album);
-            fo.write(bytes.toByteArray());
-            fo.close();
-        } catch (IOException e) {
-            Log.e(TAG, "Failed to save image in " + albumPath);
-        }
-
-        // Salva na MediaStore
-        Uri uri;
-        try
-        {
-            uri = activity.getContentResolver().insert(Images.Media.EXTERNAL_CONTENT_URI, values);
-        }
-        catch (final Exception e)
-        {
-            Log.e(TAG, "Failed to insert photo into MediaStore");
-            e.printStackTrace();
-            // Since the insertion failed, delete the photo.
-            File photo = new File(photoPath);
-            if (!photo.delete())
-            {
-                Log.e(TAG, "Failed to delete non-inserted photo");
+    public void OnActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == activity.RESULT_OK) {
+            // Determina o caminho e os metadados da foto
+            final long currentTimeMillis = System.currentTimeMillis();
+            final String appName = activity.getString(R.string.app_name);
+            final String galleryPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
+            final String albumPath = galleryPath + "/" + appName + "/" + folderName;
+            final String photoPath = albumPath + "/" + currentTimeMillis + ".png";
+            final ContentValues values = new ContentValues();
+            values.put(MediaStore.MediaColumns.DATA, photoPath);
+            values.put(MediaStore.Images.Media.MIME_TYPE, PHOTO_MIME_TYPE);
+            values.put(Images.Media.TITLE, appName);
+            values.put(Images.Media.DESCRIPTION, appName);
+            values.put(Images.Media.DATE_TAKEN, currentTimeMillis);
+            // Cria o diretório
+            File album = new File(albumPath);
+            if (!album.isDirectory() && !album.mkdirs()) {
+                Log.e(TAG, "Failed to create album directory at " + albumPath);
+                return;
             }
-            return;
+            // Pega a foto
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, bytes);
+            //Salva a foto no arquivo
+            try {
+                album.createNewFile();
+                FileOutputStream fo = new FileOutputStream(album);
+                fo.write(bytes.toByteArray());
+                fo.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Failed to save image in " + albumPath);
+            }
+
+            // Salva na MediaStore
+            Uri uri;
+            try {
+                uri = activity.getContentResolver().insert(Images.Media.EXTERNAL_CONTENT_URI, values);
+            } catch (final Exception e) {
+                Log.e(TAG, "Failed to insert photo into MediaStore");
+                e.printStackTrace();
+                // Since the insertion failed, delete the photo.
+                File photo = new File(photoPath);
+                if (!photo.delete()) {
+                    Log.e(TAG, "Failed to delete non-inserted photo");
+                }
+                return;
+            }
         }
     }
 }
